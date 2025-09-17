a1a420955c41a2cfa7253ff87fb674d0fc85750d
DiegoCortesMetacontrol <91231741+ByAncort@users.noreply.github.com>
2025-09-16 21:37:37 -0300

    feat: Update Header and add Footer component
    - Refactored Header component with a new SVG logo and improved layout.
    - Added Footer component with contact information, social media links, and company details.
    - Updated welcome page to include the new Footer component and adjusted product images.

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -5,6 +5,7 @@ import HeroSection from "~/component/HeroSelection";
 import Header from "~/component/Header";
 import ClientCarousel from "~/component/ClientCarousel";
 import CardVentas from "~/component/CardVentas";
+import Footer from "~/component/Footer";
 
 
 
@@ -18,7 +19,8 @@ const productosMasVendidos = [
     colores: 1,
     calificacion: 5,
     reseñas: 2,
-    imagen: "https://www.staycoldapparel.com/cdn/shop/files/DeadBloom_Acid_-OversizedHoodie_350GSM_4.jpg"
+    imagen: "https://www.staycoldapparel.com/cdn/shop/files/DeadBloom_Acid_-OversizedHoodie_350GSM_10.jpg",
+    imagenSecundaria: "https://www.staycoldapparel.com/cdn/shop/files/DeadBloom_Acid_-OversizedHoodie_350GSM_4.jpg"
   },
   {
     id: 2,
@@ -28,7 +30,8 @@ const productosMasVendidos = [
     colores: 1,
     calificacion: 5,
     reseñas: 5,
-    imagen: "https://www.staycoldapparel.com/cdn/shop/videos/c/vp/a3dffb6a8e494a72a65ee6d6bab07d55/a3dffb6a8e494a72a65ee6d6bab07d55.HD-1080p-7.2Mbps-57257139.mp4?v=0"
+    imagen: "https://www.staycoldapparel.com/cdn/shop/files/NocturnalOath-OversizedT-Shirt_allover_200GSM10.jpg",
+    imagenSecundaria: "staycoldapparel.com/cdn/shop/files/NocturnalOath-OversizedT-Shirt_allover_200GSM1.jpg"
   },
   {
     id: 3,
@@ -38,7 +41,8 @@ const productosMasVendidos = [
     colores: 1,
     calificacion: 4.5,
     reseñas: 7,
-    imagen: "https://www.staycoldapparel.com/cdn/shop/files/Think_Twice_Oversized_Tee_White_5.jpg"
+    imagen: "https://www.staycoldapparel.com/cdn/shop/files/ReignOfBloodGreyEdition-HeavyOversizedHoodie_400GSM_2_fcf8ddaa-ac2c-466d-a99b-851f4cc90413.jpg",
+    imagenSecundaria: "https://www.staycoldapparel.com/cdn/shop/files/ReignOfBloodGreyEdition-HeavyOversizedHoodie_400GSM_5_b4fb4203-b74f-4fcb-beb0-73b9a6580af9.jpg"
   },
   {
     id: 4,
@@ -48,7 +52,8 @@ const productosMasVendidos = [
     colores: 2,
     calificacion: 5,
     reseñas: 3,
-    imagen: "https://www.staycoldapparel.com/cdn/shop/files/ReignofBloodHoodie__ayleeshyoung5.jpg"
+    imagen: "https://www.staycoldapparel.com/cdn/shop/files/Grimfall_HalfHalf_-CargoShort_260GSM_3copy-2.jpg",
+    imagenSecundaria: "https://www.staycoldapparel.com/cdn/shop/files/Grimfall_HalfHalf_-CargoShort_260GSM_20-2.jpg"
   }
 ];
 
@@ -81,12 +86,14 @@ const productosMasVendidos = [
     <div className="snap-center">
       <Header/>
       </div>
-    <div className="snap-center">
+      <div className="snap-center">
       {/* <ClientCarousel clients={clientLogos}/> */}
       <CardVentas productos={productosMasVendidos} />
       </div>
     <div className="snap-center">
+      <Footer/>
       </div>
+
     </main>
   );
 }
