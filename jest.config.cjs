b22715efbd753540f4da443966ab37b8489c80a0
DiegoCortesMetacontrol <91231741+ByAncort@users.noreply.github.com>
2025-10-14 13:34:38 -0300

    Add Jest configuration and coverage reporting setup
    - Created a new Jest configuration file (jest.config.cjs) to set up the testing environment.
    - Configured Jest to use jsdom as the test environment.
    - Specified setup files to run after the environment is set up.
    - Added module name mapping for styles and application paths.
    - Configured Babel for transforming JavaScript and TypeScript files.
    - Excluded certain paths from test coverage collection.
    - Set up coverage collection for application files while excluding type definition files and the main index files.

---
+++ jest.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: "jsdom",
+
+  
+  setupFilesAfterEnv: ["<rootDir>/app/setupTests.js"],
+
+  
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^~/(.*)$": "<rootDir>/app/$1",
+  },
+
+  
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+
+  
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+
+  
+  collectCoverageFrom: [
+    "app/**/*.{js,jsx,ts,tsx}",
+    "!app/**/*.d.ts",
+    "!app/index.{js,ts,tsx}",
+  ],
+
+  
+  transformIgnorePatterns: [
+    "node_modules/(?!(your-es-module-packages)/)",
+  ],
+};
